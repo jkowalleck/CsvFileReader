a13950ad7f1ab08353b5c34a23cfef1cc93b4262
Jan Kowalleck <jan.kowalleck@gfk.com>
2013-11-26 20:04:13 +0100

    fixed trailingOpenLine on fileEnd

--- CsvFileReader.js
+++ CsvFileReader.js
@@ -1,5 +1,5 @@
 
-CsvFileReader = function (delimiter, enclose, escape) 
+CsvFileReader = function (delimiter, enclose, escape)
 {
 	this.delimiter = delimiter;
 	this.enclose = enclose;
@@ -10,47 +10,47 @@ CsvFileReader = function (delimiter, enclose, escape)
 
 CsvFileReader.prototype = {
 	constructor : CsvFileReader ,
-	
-	reset : function () 
+
+	reset : function ()
 	{
 		this.error = null;
 		this.data = [];
 	} ,
-	
-	read : function ( file ) 
+
+	read : function ( file )
 	{
 		var csvFileReader = this;
 		csvFileReader.reset();
-		
+
 		var reader = new LinewiseFileReader();
 		var parser = new CsvParser(this.delimiter, this.enclose, this.escape);
-		
-		// @XXX maybe make reader.bufferSize smaller ...  
-		
-		reader.onerror = function (error) 
+
+		// @XXX maybe make reader.bufferSize smaller ...
+
+		reader.onerror = function (error)
 		{
 			csvFileReader.error = this.error;
 			csvFileReader.onerror && csvFileReader.onerror(error);
 		};
-		
+
 		var lastTrailingOpenLine=false;
 		reader.onload = function (load)
 		{
 			var lines = this.lines;
 			this.reset();
-			
+
 			var trailingOpenLine = parser.parseLines(lines)
 			  , data = parser.data;
 			parser.reset();
-			
-			if ( lastTrailingOpenLine !== false ) 
+
+			if ( lastTrailingOpenLine !== false )
 			{
 				var dataFirstLine = data.shift();
-				if ( dataFirstLine )
+				if ( dataFirstLine !== undefined )
 				{
-					csvFileReader.data.push( lastTrailingOpenLine.concat(dataFirstLine) );
+					data.unshift( lastTrailingOpenLine.concat(dataFirstLine) );
 				}
-				else if ( trailingOpenLine !== false  ) 
+				else if ( trailingOpenLine !== false  )
 				{
 					trailingOpenLine = lastTrailingOpenLine.concat(trailingOpenLine);
 				}
@@ -59,26 +59,31 @@ CsvFileReader.prototype = {
 					trailingOpenLine = lastTrailingOpenLine;
 				}
 			}
-			
+
 			csvFileReader.data = csvFileReader.data.concat(data);
-			
+
+			var notDone = ( load.loaded < load.total );
+			if ( notDone )
+			{
+				lastTrailingOpenLine = trailingOpenLine;
+			}
+			else if ( trailingOpenLine !== false )
+			{
+				csvFileReader.data.push(trailingOpenLine);
+			}
+
 			var notAbort = ( csvFileReader.onload && csvFileReader.onload(load) );
-			lastTrailingOpenLine = ( notAbort ? trailingOpenLine : false );
 			return notAbort;
 		};
-		
+
 		reader.onloadend = function (loadend)
 		{
-			if ( lastTrailingOpenLine ) 
-			{
-				csvFileReader.data.push(lastTrailingOpenLine);
-			}
 			csvFileReader.onloadend && csvFileReader.onloadend(loadend);
 		};
-		
+
 		reader.read(file);
 	} ,
-	
+
 	onerror : null ,
 	onload : null , // return false to abort loading process
 	onloadend : null
